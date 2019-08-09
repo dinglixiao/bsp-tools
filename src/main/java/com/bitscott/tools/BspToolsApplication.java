b0efc2db2319fb847259ac64a0116f2fc00ac16f
dinglixiao <1713716789@qq.com>
2019-08-09 11:19:56 +0800

    Initial commit

---
+++ src/main/java/com/bitscott/tools/BspToolsApplication.java
@@ -0,0 +1,13 @@
+package com.bitscott.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BspToolsApplication.class, args);
+    }
+
+}
